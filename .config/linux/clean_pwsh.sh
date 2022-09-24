80f64215221f2a8e5c9a862c9c02fd67e7703afd
Szymon Osiecki <szymonos@outlook.com>
2022-09-24 23:17:11 +0200

    fix: uninstall powershell in Linux

--- .config/linux/clean_pwsh.sh
+++ .config/linux/clean_pwsh.sh
@@ -7,10 +7,24 @@ if [[ $(id -u) -eq 0 ]]; then
   exit 1
 fi
 
+# delete files and folders
 rm -fr ~/.config/powershell 2>/dev/null
 rm -fr ~/.local/share/powershell 2>/dev/null
-sudo rm -fr /opt/microsoft/powershell 2>/dev/null
 sudo rm -fr /usr/local/share/powershell 2>/dev/null
 sudo rm -f /etc/profile.d/theme.omp.json 2>/dev/null
 sudo rm -f /usr/bin/pwsh 2>/dev/null
 sudo rm -f /usr/bin/oh-my-posh 2>/dev/null
+
+# determine system id
+SYS_ID=$(grep -oPm1 '^ID(_LIKE)?=.*\K(alpine|arch|fedora|debian|ubuntu|opensuse)' /etc/os-release)
+case $SYS_ID in
+fedora)
+  sudo dnf remove -y powershell
+  ;;
+debian | ubuntu)
+  sudo dpkg -r powershell
+  ;;
+*)
+  sudo rm -fr /opt/microsoft/powershell 2>/dev/null
+  ;;
+esac

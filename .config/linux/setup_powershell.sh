67127a9262ef671a9ac653d5a376f027c242a996
Szymon Osiecki <szymonos@outlook.com>
2022-10-09 19:36:08 +0200

    fix(sh): pwsh installation

--- .config/linux/setup_powershell.sh
+++ .config/linux/setup_powershell.sh
@@ -19,11 +19,13 @@ sudo .config/linux/scripts/install_pwsh.sh
 
 # *Copy assets
 # calculate variables
-OMP_PROFILE=$([[ "$1" = 'pl' ]] && '.config/.assets/theme-pl.omp.json' || '.config/.assets/theme.omp.json')
+if [[ "$1" = 'pl' ]]; then
+  OMP_PROFILE='.config/.assets/theme-pl.omp.json'
+else
+  OMP_PROFILE='.config/.assets/theme.omp.json'
+fi
 PROFILE_PATH=$(pwsh -nop -c '[IO.Path]::GetDirectoryName($PROFILE.AllUsersAllHosts)')
-SCRIPTS_PATH=$(pwsh -nop -c '$env:PSModulePath.Split(':')[1].Replace('Modules', 'Scripts')')
-# create scripts folder
-sudo mkdir -p $SCRIPTS_PATH
+SCRIPTS_PATH=$(pwsh -nop -c '$env:PSModulePath.Split(":")[1].Replace("Modules", "Scripts")')
 
 # oh-my-posh profile
 sudo \cp -f "$OMP_PROFILE" "$PROFILE_PATH/theme.omp.json"
@@ -37,9 +39,11 @@ if type git &>/dev/null; then
   sudo \cp -f .config/.assets/ps_aliases_git.ps1 $SCRIPTS_PATH
 fi
 # kubectl functions
-if type kubectl &>/dev/null; then
+if type -f kubectl &>/dev/null; then
   sudo \cp -f .config/.assets/ps_aliases_kubectl.ps1 $SCRIPTS_PATH
   # add powershell kubectl autocompletion
-  pwsh -nop -c '(kubectl completion powershell).Replace("''kubectl''", "''k''") > $PROFILE.CurrentUserAllHosts'
-
+  cat <<'EOF' | pwsh -nop -c -
+$kctl = Get-Command kubectl -All | Where-Object -Property Version | Select-Object -First 1 -ExpandProperty Source
+(& $kctl completion powershell).Replace("'kubectl'", "'k'") >$PROFILE.CurrentUserAllHosts'
+EOF
 fi

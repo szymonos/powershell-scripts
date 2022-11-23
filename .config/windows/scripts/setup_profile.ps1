1c14a0c5f102d17adc8831b5c59f9ef65eebfb50
Szymon Osiecki <szymonos@outlook.com>
2022-11-23 08:52:41 +0100

    fix: setup_profile win

--- .config/windows/scripts/setup_profile.ps1
+++ .config/windows/scripts/setup_profile.ps1
@@ -8,6 +8,5 @@ pwsh -NoProfile -Command 'Install-Module PowerShellGet -AllowPrerelease -Force -
 
 pwsh -NoProfile -Command @'
 Write-Host "Set PSGallery Trusted" && Set-PSResourceRepository -Name PSGallery -Trusted -WarningAction SilentlyContinue;
-Write-Host "Install PSReadLine" && Install-PSResource -Name PSReadLine -Quiet -WarningAction SilentlyContinue;
 Write-Host "Install posh-git" && Install-PSResource -Name posh-git -Quiet -WarningAction SilentlyContinue
 '@

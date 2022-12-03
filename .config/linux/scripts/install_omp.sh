24df2314f1b92d0142e2b8e64a1acab23d0d9bcb
Szymon Osiecki <szymonos@outlook.com>
2022-12-03 23:45:33 +0100

    refactor: pwsh linux install

--- .config/linux/scripts/install_omp.sh
+++ .config/linux/scripts/install_omp.sh
@@ -2,23 +2,27 @@
 : '
 sudo .config/linux/scripts/install_omp.sh
 '
+if [[ $EUID -ne 0 ]]; then
+  echo -e '\e[91mRun the script with sudo!\e[0m'
+  exit 1
+fi
 
 APP='oh-my-posh'
 # get latest release if not provided as a parameter
 while [[ -z "$REL" ]]; do
   REL=$(curl -sk https://api.github.com/repos/JanDeDobbeleer/oh-my-posh/releases/latest | grep -Po '"tag_name": *"v\K.*?(?=")')
-  [ -n "$REL" ] || echo 'retrying...'
+  [[ -n "$REL" ]] || echo 'retrying...' >&2
 done
 
 if type $APP &>/dev/null; then
   VER=$(oh-my-posh version)
   if [ "$REL" = "$VER" ]; then
-    echo -e "\e[36m$APP v$VER is already latest\e[0m"
+    echo -e "\e[36m$APP v$VER is already latest\e[0m" >&2
     exit 0
   fi
 fi
 
-echo -e "\e[96minstalling $APP v$REL\e[0m"
+echo -e "\e[96minstalling $APP v$REL\e[0m" >&2
 while [[ ! -f posh-linux-amd64 ]]; do
   curl -LsOk 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64'
 done

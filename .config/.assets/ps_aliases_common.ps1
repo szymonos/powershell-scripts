e50fb3d2f969fbad7f8abde24ae1a8350cf08170
Szymon Osiecki <szymonos@outlook.com>
2022-12-04 11:11:22 +0100

    refactor: remove module functions from aliases

--- .config/.assets/ps_aliases_common.ps1
+++ .config/.assets/ps_aliases_common.ps1
@@ -4,14 +4,7 @@ function ... { Set-Location ../../ }
 function .... { Set-Location ../../../ }
 function src { . $PROFILE.CurrentUserAllHosts }
 function la { Get-ChildItem @args -Force }
-function Get-CmdAlias ([string]$CmdletName) {
-    Get-Alias | `
-        Where-Object -FilterScript { $_.Definition -match $CmdletName } | `
-        Sort-Object -Property Definition, Name | `
-        Select-Object -Property Definition, Name
-}
 
 # *Aliases
-Set-Alias -Name alias -Value Get-CmdAlias
 Set-Alias -Name c -Value Clear-Host
 Set-Alias -Name type -Value Get-Command

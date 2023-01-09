0a1bccaab6c4022927c13f2a0c2a254b634dcc99
Szymon Osiecki <szymonos@outlook.com>
2023-01-09 22:46:14 +0100

    refactor: exa alias

--- .config/.assets/pwsh_cfg/ps_aliases_macos.ps1
+++ .config/.assets/pwsh_cfg/ps_aliases_macos.ps1
@@ -4,7 +4,7 @@ function less { $input | & /usr/bin/env less -FRXc @args }
 function ls { & /usr/bin/env ls --color=auto @args }
 function l { ls -1 @args }
 function lsa { ls -lah @args }
-function ll { & /usr/bin/env exa -lagh --color=auto --time-style=long-iso --group-directories-first @args }
+function ll { & /usr/bin/env exa -lagh --git --time-style=long-iso --group-directories-first @args }
 function md { mkdir -p @args }
 function mkdir { & /usr/bin/env mkdir -pv @args }
 function mv { & /usr/bin/env mv -iv @args }

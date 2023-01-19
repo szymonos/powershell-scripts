7c803d28975ed68219e73bbf7b3678f5fb158f84
Szymon Osiecki <szymonos@outlook.com>
2023-01-19 21:46:42 +0100

    fix: macos config

--- .config/macos/setup_powershell.sh
+++ .config/macos/setup_powershell.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 : '
-.config/macos/setup_powershell.sh --theme powerline --ps_modules "do-common"
+.config/macos/setup_powershell.sh --theme nerd --ps_modules "do-common do-linux"
 '
 if [[ $EUID -eq 0 ]]; then
-  echo -e '\e[91mDo not run the script as root!\e[0m'
+  printf '\e[91mDo not run the script as root!\e[0m\n'
   exit 1
 fi
 
@@ -22,23 +22,23 @@ done
 WORKSPACE_FOLDER=$(dirname "$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")")
 [[ "$PWD" = "$WORKSPACE_FOLDER" ]] || cd "$WORKSPACE_FOLDER"
 
-echo -e "\e[32minstalling brew...\e[0m"
+printf "\e[32minstalling brew...\e[0m\n"
 .config/macos/scripts/install_brew.sh
-echo -e "\e[32minstalling pwsh packages...\e[0m"
+printf "\e[32minstalling pwsh packages...\e[0m\n"
 .config/macos/scripts/install_exa.sh
 .config/macos/scripts/install_omp.sh
 .config/macos/scripts/install_pwsh.sh
-echo -e "\e[32msetting up profile for all users...\e[0m"
+printf "\e[32msetting up profile for all users...\e[0m\n"
 sudo .config/linux/scripts/setup_omp.sh --theme $theme
 sudo .config/macos/scripts/setup_profile_allusers.ps1
-echo -e "\e[32msetting up profile for current user...\e[0m"
+printf "\e[32msetting up profile for current user...\e[0m\n"
 .config/linux/scripts/setup_profile_user.ps1
 if [[ -n "$ps_modules" ]]; then
   if [ ! -d ../ps-modules ]; then
     remote=$(git config --get remote.origin.url)
     git clone ${remote/powershell-scripts/ps-modules} ../ps-modules
   fi
-  echo -e "\e[32minstalling PowerShell modules...\e[0m"
+  printf "\e[32minstalling PowerShell modules...\e[0m\n"
   mods=($ps_modules)
   for mod in ${mods[@]}; do
     if [ "$mod" = 'do-common' ]; then

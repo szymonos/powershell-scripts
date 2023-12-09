b224156c6de7bc3f4421892c4acc114cecd85ee4
Szymon Osiecki <szymonos@outlook.com>
2023-12-09 12:17:16 +0100

    refactor: repace exa with eza

--- .config/pwsh_cfg/_aliases_linux.ps1
+++ .config/pwsh_cfg/_aliases_linux.ps1
@@ -7,16 +7,23 @@ if ($env:DISTRO_FAMILY -eq 'alpine') {
     function ip { $input | & /usr/bin/env ip --color=auto @args }
     function ls { & /usr/bin/env ls -h --color=auto --group-directories-first --time-style=long-iso @args }
 }
-function exa { & /usr/bin/env exa -g --color=auto --time-style=long-iso --group-directories-first @args }
-function ll { exa -lah @args }
-function grep { $input | & /usr/bin/env grep --color=auto @args }
+function eza { & /usr/bin/env eza -g --color=auto --time-style=long-iso --group-directories-first --color-scale=all --git-repos @args }
+function l { eza -1 @args }
+function lsa { eza -a @args }
+function ll { eza -lah @args }
+function lt { eza -Th @args }
+function lta { eza -aTh --git-ignore @args }
+function ltd { eza -DTh @args }
+function ltad { eza -aDTh --git-ignore @args }
+function llt { eza -lTh @args }
+function llta { eza -laTh --git-ignore @args }
+function grep { $input | & /usr/bin/env grep --ignore-case --color=auto @args }
 function less { $input | & /usr/bin/env less -FRXc @args }
-function l { ls -1 @args }
-function lsa { ls -la @args }
 function mkdir { & /usr/bin/env mkdir -pv @args }
 function mv { & /usr/bin/env mv -iv @args }
 function nano { & /usr/bin/env nano -W @args }
 function p { & /usr/bin/env pwsh -NoProfileLoadTime @args }
+function rg { $input | & /usr/bin/env rg --ignore-case @args }
 function tree { & /usr/bin/env tree -C @args }
 function wget { & /usr/bin/env wget -c @args }
 
--- .config/pwsh_cfg/_aliases_win.ps1
+++ .config/pwsh_cfg/_aliases_win.ps1
@@ -1,4 +1,19 @@
 # *Aliases
 Set-Alias -Name _ -Value gsudo
 Set-Alias -Name ff -Value fastfetch
-Set-Alias -Name ll -Value Get-ChildItem
+
+# *eza functions
+if (Get-Command eza.exe -CommandType Application -ErrorAction SilentlyContinue) {
+    function exa { eza -g --color=auto --time-style=long-iso --group-directories-first --color-scale=all --git --icons --git-repos @args }
+    function l { exa -1 @args }
+    function lsa { exa -a @args }
+    function ll { exa -lah @args }
+    function lt { exa -Th @args }
+    function lta { exa -aTh --git-ignore @args }
+    function ltd { exa -DTh @args }
+    function ltad { exa -aDTh --git-ignore @args }
+    function llt { exa -lTh @args }
+    function llta { exa -laTh --git-ignore @args }
+} else {
+    function ll { Get-ChildItem @args -Force }
+}
